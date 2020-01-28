68235e70226e7bfd890c037132a2bf02dae33809
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2020-01-28 22:45:49 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # tells Rails routing system that the route shoule bd passed through students_controller's '/students' action 
+  # a rails route maps to a URL to a controller and Action
+  # get 'students', action: :index, controller: 'students'
+  get 'students', to: 'students#index'
 end
